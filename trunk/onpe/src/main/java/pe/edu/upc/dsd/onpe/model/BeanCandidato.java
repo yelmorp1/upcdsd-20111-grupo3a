b6598247729236b5513c427b342845540915f237
lunartanis@gmail.com <lunartanis@gmail.com@ecf35a0d-18e5-c912-576c-fa5d76cb1bce>
2011-05-17 01:12:36 +0000

    - Agregar atributos (BeanCandidato)

--- trunk/onpe/src/main/java/pe/edu/upc/dsd/onpe/model/BeanCandidato.java
+++ trunk/onpe/src/main/java/pe/edu/upc/dsd/onpe/model/BeanCandidato.java
@@ -1,5 +1,28 @@
 package pe.edu.upc.dsd.onpe.model;
 
 public class BeanCandidato {
-
+	
+	private String nombre;
+	private String apellidos;
+	private String partidoPolitico;
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setPartidoPolitico(String partidoPolitico) {
+		this.partidoPolitico = partidoPolitico;
+	}
+	public String getPartidoPolitico() {
+		return partidoPolitico;
+	}	
+	
 }
