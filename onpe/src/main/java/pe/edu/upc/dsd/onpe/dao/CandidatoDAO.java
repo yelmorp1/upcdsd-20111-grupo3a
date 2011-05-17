43f1542419116d76aa9fb4dc6ab0677fd5789d2e
priospino <priospino@gmail.com>
2011-05-17 01:13:18 +0000

    Interface Candidato

---
+++ onpe/src/main/java/pe/edu/upc/dsd/onpe/dao/CandidatoDAO.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.dsd.onpe.dao;
+
+import java.util.List;
+
+import pe.edu.upc.dsd.onpe.model.BeanCandidato;
+
+public interface CandidatoDAO {
+	
+	
+	public List<BeanCandidato> getListaCandidatos();
+}
