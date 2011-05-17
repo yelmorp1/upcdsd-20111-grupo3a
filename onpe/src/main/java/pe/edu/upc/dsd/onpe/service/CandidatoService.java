b021b5a7ef98470f2f26c9334370641fcb170202
priospino <priospino@gmail.com>
2011-05-17 01:15:14 +0000

    Service Candidato

---
+++ onpe/src/main/java/pe/edu/upc/dsd/onpe/service/CandidatoService.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.dsd.onpe.service;
+
+import java.util.List;
+
+import pe.edu.upc.dsd.onpe.model.BeanCandidato;
+
+public interface CandidatoService {
+
+	public List<BeanCandidato> getListaCandidatos();
+}
