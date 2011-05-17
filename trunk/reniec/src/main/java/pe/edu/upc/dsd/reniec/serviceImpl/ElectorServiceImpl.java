c7044548448781e47485d46fc811e9af60788e50
alyssa.quinto@gmail.com <alyssa.quinto@gmail.com@ecf35a0d-18e5-c912-576c-fa5d76cb1bce>
2011-05-17 01:45:51 +0000

    reniec

---
+++ trunk/reniec/src/main/java/pe/edu/upc/dsd/reniec/serviceImpl/ElectorServiceImpl.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.dsd.reniec.serviceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.edu.upc.dsd.reniec.model.BeanElector;
+import pe.edu.upc.dsd.reniec.service.ElectorService;
+
+public class ElectorServiceImpl implements ElectorService{
+
+	public List<BeanElector> getListaElector() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
