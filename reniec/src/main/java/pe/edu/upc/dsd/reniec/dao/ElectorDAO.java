d068face9745feca88dc4d87b9d0e0eafdee0be8
alyssa.quinto <alyssa.quinto@gmail.com>
2011-05-17 01:45:51 +0000

    reniec

---
+++ reniec/src/main/java/pe/edu/upc/dsd/reniec/dao/ElectorDAO.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.dsd.reniec.dao;
+
+import java.util.List;
+
+import pe.edu.upc.dsd.reniec.model.BeanElector;
+
+public interface ElectorDAO {
+
+	public List<BeanElector> getListaElector();
+	
+}
---
+++ reniec/src/main/java/pe/edu/upc/dsd/reniec/service/ElectorService.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.dsd.reniec.service;
+
+import java.util.List;
+
+import pe.edu.upc.dsd.reniec.model.BeanElector;
+import pe.edu.upc.dsd.reniec.model.BeanElector;;
+
+public interface ElectorService {
+
+	public List<BeanElector> getListaElector();
+	
+}
