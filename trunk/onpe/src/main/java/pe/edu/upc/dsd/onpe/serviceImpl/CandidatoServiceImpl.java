29ea49254730a25cd0647245b6c7bcddde47dbd8
priospino@gmail.com <priospino@gmail.com@ecf35a0d-18e5-c912-576c-fa5d76cb1bce>
2011-05-17 01:15:14 +0000

    Service Candidato

---
+++ trunk/onpe/src/main/java/pe/edu/upc/dsd/onpe/serviceImpl/CandidatoServiceImpl.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.dsd.onpe.serviceImpl;
+
+import java.util.List;
+
+import pe.edu.upc.dsd.onpe.model.BeanCandidato;
+import pe.edu.upc.dsd.onpe.service.CandidatoService;
+
+public class CandidatoServiceImpl implements CandidatoService{
+
+	public List<BeanCandidato> getListaCandidatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
