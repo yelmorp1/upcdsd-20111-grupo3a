7e254a78b3b6ac43b8f90695c0ae0a50d8b3209e
alyssa.quinto <alyssa.quinto@ecf35a0d-18e5-c912-576c-fa5d76cb1bce>
2011-05-27 17:13:53 +0000

    nodo reniec

--- reniec/src/main/java/pe/edu/upc/dsd/reniec/model/BeanElector.java
+++ reniec/src/main/java/pe/edu/upc/dsd/reniec/model/BeanElector.java
@@ -5,6 +5,21 @@ public class BeanElector {
 	private String dni;
 	private String nombre;
 	private String apellidos;
+	private String pin;
+	private String clave;
+	
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
 	public String getDni() {
 		return dni;
 	}
--- reniec/src/main/java/pe/edu/upc/dsd/reniec/serviceImpl/ElectorServiceImpl.java
+++ reniec/src/main/java/pe/edu/upc/dsd/reniec/serviceImpl/ElectorServiceImpl.java
@@ -8,8 +8,250 @@ import pe.edu.upc.dsd.reniec.service.ElectorService;
 
 public class ElectorServiceImpl implements ElectorService{
 
-	public List<BeanElector> getListaElector() {
+	public List<BeanElector> getListaElectores() {
 		// TODO Auto-generated method stub
-		return null;
+		List  lstElectores = new ArrayList<BeanElector>();
+		
+		BeanElector elector1 = new BeanElector();
+		elector1.setDni("199820014");
+		elector1.setNombre("RAFAEL");
+		elector1.setApellidos("MICHUY GUTIERREZ");
+		elector1.setPin("199820014");
+		elector1.setClave("123456");
+		lstElectores.add(elector1);
+		
+		BeanElector elector2 = new BeanElector();
+		elector2.setDni("200010109");
+		elector2.setNombre("JORGE CARLOS");
+		elector2.setApellidos("BAZAN JARA");
+		elector2.setPin("200010109");
+		elector2.setClave("123456");
+		lstElectores.add(elector2);
+				
+		BeanElector elector3 = new BeanElector();
+		elector3.setDni("200520495");
+		elector3.setNombre("JOSE AUGUSTO");
+		elector3.setApellidos("PINGLO ORTIZ");
+		elector3.setPin("200520495");
+		elector3.setClave("123456");
+		lstElectores.add(elector3);
+		
+		BeanElector elector4 = new BeanElector();
+		elector4.setDni("200611937");
+		elector4.setNombre("JOSE ANTONIO");
+		elector4.setApellidos("BONILLA NOVARO");
+		elector4.setPin("200611937");
+		elector4.setClave("123456");
+		lstElectores.add(elector4);
+		
+		BeanElector elector5 = new BeanElector();
+		elector5.setDni("200620616");
+		elector5.setNombre("RAUL CESAR");
+		elector5.setApellidos("GIL MUÑOZ");
+		elector5.setPin("200620616");
+		elector5.setClave("123456");
+		lstElectores.add(elector5);
+		
+		BeanElector elector6 = new BeanElector();
+		elector6.setDni("200712506");
+		elector6.setNombre("FRANK");
+		elector6.setApellidos("CASTRO TAYA");
+		elector6.setPin("200712506");
+		elector6.setClave("123456");
+		lstElectores.add(elector6);
+				
+		BeanElector elector7 = new BeanElector();
+		elector7.setDni("200712517");
+		elector7.setNombre("CARLOS LUIS");
+		elector7.setApellidos("SANCHEZ GARCIA");
+		elector7.setPin("200712517");
+		elector7.setClave("123456");
+		lstElectores.add(elector7);
+		
+		BeanElector elector8 = new BeanElector();
+		elector8.setDni("200712655");
+		elector8.setNombre("YOLANDA CECILIA");
+		elector8.setApellidos("MANCHA PAREDES");
+		elector8.setPin("200712655");
+		elector8.setClave("123456");
+		lstElectores.add(elector8);
+		
+		BeanElector elector9 = new BeanElector();
+		elector9.setDni("200800168");
+		elector9.setNombre("ALBERTO FELIX");
+		elector9.setApellidos("FLORES MENDOZA");
+		elector9.setPin("200800168");
+		elector9.setClave("123456");
+		lstElectores.add(elector9);
+		
+		BeanElector elector10 = new BeanElector();
+		elector10.setDni("200814303");
+		elector10.setNombre("LUIS ANTONIO");
+		elector10.setApellidos("AYALA OLIVARES");
+		elector10.setPin("200814303");
+		elector10.setClave("123456");
+		lstElectores.add(elector10);
+				
+		BeanElector elector11 = new BeanElector();
+		elector11.setDni("200821047");
+		elector11.setNombre("WILLIAM ROBERT");
+		elector11.setApellidos("REQUEJO SANTOS");
+		elector11.setPin("200821047");
+		elector11.setClave("123456");
+		lstElectores.add(elector11);
+		
+		BeanElector elector12 = new BeanElector();
+		elector12.setDni("200900612");
+		elector12.setNombre("FERNANDO MOISES");
+		elector12.setApellidos("ZEGARRA GARCIA");
+		elector12.setPin("200900612");
+		elector12.setClave("123456");
+		lstElectores.add(elector12);
+		
+		BeanElector elector13 = new BeanElector();
+		elector13.setDni("200900640");
+		elector13.setNombre("ITALA JUDITH");
+		elector13.setApellidos("VILLAGOMEZ ALVARADO");
+		elector13.setPin("200900640");
+		elector13.setClave("123456");
+		lstElectores.add(elector13);
+		
+		BeanElector elector14 = new BeanElector();
+		elector14.setDni("200913815");
+		elector14.setNombre("ALEJANDRO PATRICIO");
+		elector14.setApellidos("MURILLO CORRALES");
+		elector14.setPin("200913815");
+		elector14.setClave("123456");
+		lstElectores.add(elector14);
+				
+		BeanElector elector15 = new BeanElector();
+		elector15.setDni("200913997");
+		elector15.setNombre("JORGE LUIS");
+		elector15.setApellidos("CHIPANA CHAVEZ");
+		elector15.setPin("200913997");
+		elector15.setClave("123456");
+		lstElectores.add(elector15);
+		
+		BeanElector elector16 = new BeanElector();
+		elector16.setDni("200920981");
+		elector16.setNombre("JORGE ARMANDO");
+		elector16.setApellidos("CAMACHO REYES");
+		elector16.setPin("200920981");
+		elector16.setClave("123456");
+		lstElectores.add(elector16);
+		
+		BeanElector elector17 = new BeanElector();
+		elector17.setDni("200921042");
+		elector17.setNombre("JOSE ALONSO");
+		elector17.setApellidos("CARDENAS MARQUEZ");
+		elector17.setPin("200921042");
+		elector17.setClave("123456");
+		lstElectores.add(elector17);
+		
+		BeanElector elector18 = new BeanElector();
+		elector18.setDni("201000073");
+		elector18.setNombre("EDUARDO FERNANDO");
+		elector18.setApellidos("MIRANDA ASSEN");
+		elector18.setPin("201000073");
+		elector18.setClave("123456");
+		lstElectores.add(elector18);
+				
+		BeanElector elector19 = new BeanElector();
+		elector19.setDni("201000144");
+		elector19.setNombre("EDUARDO JUNIOR JOSE");
+		elector19.setApellidos("RUFASTO GOCHE");
+		elector19.setPin("201000144");
+		elector19.setClave("123456");
+		lstElectores.add(elector19);
+		
+		BeanElector elector20 = new BeanElector();
+		elector20.setDni("201000261");
+		elector20.setNombre("JUAN RODRIGO");
+		elector20.setApellidos("HUMIRE MEZA");
+		elector20.setPin("201000261");
+		elector20.setClave("123456");
+		lstElectores.add(elector20);
+				
+		BeanElector elector21 = new BeanElector();
+		elector21.setDni("201000312");
+		elector21.setNombre("ANDRES");
+		elector21.setApellidos("DEZA VILLACORTA");
+		elector21.setPin("201000312");
+		elector21.setClave("123456");
+		lstElectores.add(elector21);
+		
+		BeanElector elector22 = new BeanElector();
+		elector22.setDni("201000313");
+		elector22.setNombre("CAROLINA BEATRIZ");
+		elector22.setApellidos("ROMERO PIZARRO");
+		elector22.setPin("201000313");
+		elector22.setClave("123456");
+		lstElectores.add(elector22);
+		
+		BeanElector elector23 = new BeanElector();
+		elector23.setDni("201000353");
+		elector23.setNombre("RICARDO");
+		elector23.setApellidos("PECHE COTERA");
+		elector23.setPin("201000353");
+		elector23.setClave("123456");
+		lstElectores.add(elector23);
+		
+		BeanElector elector24 = new BeanElector();
+		elector24.setDni("201013771");
+		elector24.setNombre("ALYSSA MARINA");
+		elector24.setApellidos("QUINTO CRUZADO");
+		elector24.setPin("201013771");
+		elector24.setClave("123456");
+		lstElectores.add(elector24);
+				
+		BeanElector elector25 = new BeanElector();
+		elector25.setDni("201013772");
+		elector25.setNombre("RALF ALEXANDER");
+		elector25.setApellidos("SALDAÑA BACALLA");
+		elector25.setPin("201013772");
+		elector25.setClave("123456");
+		lstElectores.add(elector25);
+		
+		BeanElector elector26 = new BeanElector();
+		elector26.setDni("201013842");
+		elector26.setNombre("JOHANA PATRICIA");
+		elector26.setApellidos("HERRERA QUINTANILLA");
+		elector26.setPin("201013842");
+		elector26.setClave("123456");
+		lstElectores.add(elector26);
+		
+		BeanElector elector27 = new BeanElector();
+		elector27.setDni("201013893");
+		elector27.setNombre("MAYRA REBECA");
+		elector27.setApellidos("MARQUEZ ROSADIO");
+		elector27.setPin("201013893");
+		elector27.setClave("123456");
+		lstElectores.add(elector27);
+		
+		BeanElector elector28 = new BeanElector();
+		elector28.setDni("201013952");
+		elector28.setNombre("PEDRO EDISON");
+		elector28.setApellidos("RIOS PINO");
+		elector28.setPin("201013952");
+		elector28.setClave("123456");
+		lstElectores.add(elector28);
+				
+		BeanElector elector29 = new BeanElector();
+		elector29.setDni("201013997");
+		elector29.setNombre("JOSE DAN");
+		elector29.setApellidos("TORRES ATALAYA");
+		elector29.setPin("201013997");
+		elector29.setClave("123456");
+		lstElectores.add(elector29);
+		
+		BeanElector elector30 = new BeanElector();
+		elector30.setDni("201014022");
+		elector30.setNombre("CARLOS GREGORIO");
+		elector30.setApellidos("ZELA BUENO");
+		elector30.setPin("201014022");
+		elector30.setClave("123456");
+		lstElectores.add(elector30);
+		
+		return lstElectores;
 	}
 }
