aa4ebb2c3fbac6946089abd03dedc636a5dd8d2e
lunartanis <lunartanis@gmail.com>
2011-06-07 00:46:15 +0000

    BN cliente

---
+++ bn-client/src/test/java/pe/edu/upc/dsd/rs/bn/client/MultaClientTest.java
@@ -0,0 +1,28 @@
+package pe.edu.upc.dsd.rs.bn.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import junit.framework.Assert;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+public class MultaClientTest {
+
+	@Test
+	public void test() throws Exception {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(
+				"http://localhost:8080/bn/rest/multas/");
+		HttpResponse response = httpclient.execute(httpget);
+		HttpEntity entity = response.getEntity();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				entity.getContent()));		
+		Assert.assertNotNull(reader.readLine());
+	}
+}
