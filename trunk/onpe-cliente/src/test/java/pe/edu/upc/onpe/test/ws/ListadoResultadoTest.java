91210a1a761fb3e0c6b515e0a79bc0b29d70c13c
just.johana@gmail.com <just.johana@gmail.com@ecf35a0d-18e5-c912-576c-fa5d76cb1bce>
2011-06-07 00:37:44 +0000

    Test cantidad de votos de Ollanta ¬¬

--- trunk/onpe-cliente/src/test/java/pe/edu/upc/onpe/test/ws/ListadoResultadoTest.java
+++ trunk/onpe-cliente/src/test/java/pe/edu/upc/onpe/test/ws/ListadoResultadoTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import pe.edu.upc.dsd.onpe.model.BeanCandidato;
+import pe.edu.upc.dsd.onpe.model.BeanResultado;
 import pe.edu.upc.dsd.onpe.ws.ServiciosWeb;
 
 public class ListadoResultadoTest {
@@ -22,4 +24,10 @@ public class ListadoResultadoTest {
 	public void testObtenerResultados() {
 		Assert.assertEquals(6, servicios.getObtenerResutados().size());
 	}
+	
+	@Test
+	public void testCantidadVotosOllanta() {
+		Assert.assertEquals(100000, ((BeanResultado)servicios.getObtenerResutados().get(2)).getCantVotos());
+	}
+	
 }
